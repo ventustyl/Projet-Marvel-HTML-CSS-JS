ace989d29be2f2946c55f69d3d6b8d6531484ecb
ventustyl <eric.venturino@orange.fr>
2022-09-22 11:33:28 +0200

    Premier Envoi
    Page Index en HTML CSS JS classic

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+/* Sélection des éléments HTML */
+let link = document.getElementById('link')
+let burger = document.getElementById('burger')
+let ul = document.querySelector('ul')
+
+/* gestionnaire d'événement sur le a#link pour venir changer l'attribution de la classe .open à la ul et au span#burger */
+link.addEventListener('click', function (e) {
+  e.preventDefault()
+  burger.classList.toggle('open')
+  ul.classList.toggle('open')
+})
+
+
+let btn = document.querySelector('.label') 
+let sortie = document.querySelector('.liste');
+let ordre = document.querySelector('.liste2');
+
+
+btn.addEventListener('click', function() {
+  sortie.classList.toggle('none')
+  ordre.classList.toggle('bloc')
+})
+
